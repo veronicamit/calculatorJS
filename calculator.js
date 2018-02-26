f0c79fa94b13452db644e310479ad1b43cfcd482
veronicamit <veronikamitova45@gmail.com>
2018-02-26 16:22:59 +0200

    calcJS

---
+++ calculator.js
@@ -0,0 +1,11 @@
+
+function calc(v1,v2,operation){
+    
+    if (operation == '*'){ 
+    return (v1*v2);
+}else if (operation =='+') {
+    return (v1+v2);
+}else if (operation =='-'){ 
+return (v1-v2);
+} else return -1;
+}
